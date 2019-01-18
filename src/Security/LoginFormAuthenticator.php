976b6bccf7b39e9cee3f0c3d745ea80b0a86c13b
Maxime Girard <a>
2019-01-18 11:56:08 +0100

    css and change route

--- src/Security/LoginFormAuthenticator.php
+++ src/Security/LoginFormAuthenticator.php
@@ -85,7 +85,8 @@ class LoginFormAuthenticator extends AbstractFormLoginAuthenticator
         }
 
         // For example : return new RedirectResponse($this->router->generate('some_route'));
-        throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        //throw new \Exception('TODO: provide a valid redirect inside '.__FILE__);
+        return new RedirectResponse($this->router->generate('public'));
     }
 
     protected function getLoginUrl()
